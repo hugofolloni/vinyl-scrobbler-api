38f3680af41b6fb330413b62aca4f3be09260714
hugofolloni <hugofollogua07@gmail.com>
2026-07-16 22:30:51 -0300

    feat: add popularity as a search parameter

---
+++ Domain/Config/SpotifySettings.cs
@@ -0,0 +1,7 @@
+namespace ScrobblerApi.Models.Config;
+
+public class SpotifySettings
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+}
--- Domain/DTOs/AlbumSearchResult.cs
+++ Domain/DTOs/AlbumSearchResult.cs
@@ -5,4 +5,5 @@ public class AlbumSearchResult
     public string Name { get; set; } = string.Empty;
     public string Artist { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
+    public int Popularity { get; set; }
 }
